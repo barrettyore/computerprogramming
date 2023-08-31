465efdb240268d167463b7c9e6731bc3679e91cb
Scholar <scholar@paradigm.local>
2023-08-31 13:26:20 -0600

    pdm is uploaded

---
+++ pdm.py
@@ -0,0 +1,29 @@
+x = "barrett"
+
+list = [1, 2, 3, 4, 5,]
+
+ditionary = {
+    'name': "barrett",
+    "Age": 15 # im 15 btw
+}
+
+is_this_a_indintifier = 3
+
+def function(number):
+    if number %2 == 0:
+        print("is even")
+    else:
+        print("not even")
+
+function(86)
+
+varible = 9
+
+MAX_HEALTH = 1000
+
+iDontLikeCamelCase = True
+
+WhatIsPascalAnyways = 1
+
+wahoo_snake_time = True
+
