98684b04e6d665aa49652ba5ffbc59086842e33d
Scholar <scholar@paradigm.local>
2023-09-05 12:37:43 -0600

    new assigment yeah

---
+++ program_devlopment_methogy.py
@@ -0,0 +1,9 @@
+name = "barrett"
+test = 20
+food = True
+classes = ["dalton kedge", "art around the world", "earth and space science", "computer proggraming", "web dev"]
+def add(number1,number2):
+    print(number1+number2)
+
+add(1,7)
+
