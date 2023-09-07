8d8343b7dd55952197c99994e37283dd5cff8ebd
Scholar <scholar@paradigm.local>
2023-09-07 13:31:38 -0600

    intro

---
+++ intro to prog.py
@@ -0,0 +1,64 @@
+wahoo = "wahoo" #this is a global varible its evrywhere wahoooooooooooooo
+
+def test():
+    teste ="test"#is local
+    print(teste)
+#print(teste) wont work not gobal you could global
+
+in_put = input("wahooooooooooooooooooooo:>")#input
+print(f"you typed {in_put}")#output
+interger = 5
+float = 3.0
+string = "9"
+print(float+interger)#will be float
+bool = False 
+
+empty=""#declearation
+my_intalize = "hello_world"#good job i knew you could type it
+print(3+6)#math
+print(3/6)#math
+print(3-6)#math
+print(3*6)#math
+print(3%6)#math
+
+x = 3
+y = 6 
+
+if x>y:#compare
+    pass
+if x<y:#compare
+    pass
+if x<=y:#compare
+    pass
+if x>=y:#compare
+    pass
+
+x = True
+y = False
+#logic
+if x or y:
+    pass
+if x and y:
+    pass
+if x and not y:
+    pass
+
+#and etc
+
+print(3/4)#int div
+print(3.0/4.0)#flt div
+
+def function():
+    print("this is a function")
+#readibillity
+thisisnotreadable = True
+this_is_not_readable = False
+
+#built in function
+x = "this will be turned in to a list at the charector"
+a = list(x)
+print(a)
+
+
+
+
