a648ad86fe46360dd7d1272fa84c51b2f038f649
Scholar <scholar@paradigm.local>
2023-10-10 12:43:37 -0600

    updated

--- rockonline_serv.py
+++ rockonline_serv.py
@@ -26,34 +26,41 @@ while True:
     print(f"connected to {adress}")
     while True:
         play1 = input("r,p, or s:>")
-        print("play2 is decided please wait")
+        print("player2 is decided please wait")
         communication_socket.send(f"!PLAY1READY".encode("utf-8"))
         message = communication_socket.recv(1024).decode("utf-8")
         play2 = message
         if play1 == play2:
-            print("tie")
-            communication_socket.send(f"tie".encode("utf-8"))
+            print("tie, you chose the same")
+            communication_socket.send(f"tie, you chose the same".encode("utf-8"))
         elif play1 == "r" and play2  == "s":
-            print("play1 won")
-            communication_socket.send(f"play1 won".encode("utf-8"))
+            print("you won, rock beats scissors")
+            communication_socket.send(f"you lost, rock beats scissors".encode("utf-8"))
+
         elif play1 == "r" and play2  == "p":
-            print("play2 won")
-            communication_socket.send(f"play2 won".encode("utf-8"))
+            print("you lost, paper beats rock")
+            communication_socket.send(f"you won, paper beats rock".encode("utf-8"))
+
         elif play1 == "p" and play2  == "r":
-            print("play1 won")
-            communication_socket.send(f"play1 won".encode("utf-8"))
+            print("you won, paper beats rock")
+            communication_socket.send(f"you lost, paper beats rock".encode("utf-8"))
+
         elif play1 == "p" and play2  == "s":
-            print("play2 won")
-            communication_socket.send(f"play2 won".encode("utf-8"))
+            print("you lost, scissors beats paper")
+            communication_socket.send(f"you won,scissors beats paper".encode("utf-8"))
+            
         elif play1 == "s" and play2  == "r":
-            print("play2 won")
-            communication_socket.send(f"play2 won".encode("utf-8"))
+            print("you lost,rock beats scissoea")
+            communication_socket.send(f"you won,rock beats scissors".encode("utf-8"))
+
         elif play1 == "s" and play2  == "p":
-            print("play1 won")
-            communication_socket.send(f"play1 won".encode("utf-8"))
+            print("you won,scissors beats paper")
+            communication_socket.send(f"you lost,scissors beats paper".encode("utf-8"))
+
         else:
-            print("unown input:>")
-            communication_socket.send(f"uknown imput".encode("utf-8"))
+            print("uknown input, one of you put in a invalid input")
+            communication_socket.send(f"uknown input, one of you put in a invalid input".encode("utf-8"))
+
         playagain = input("play again y/n:>")
         if playagain != "y":
             communication_socket.send(f"host has ended the game".encode("utf-8"))  
